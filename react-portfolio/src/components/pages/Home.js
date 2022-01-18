3c2f3ef6f9c792bed500fa09ea9e72ef5d917545
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2022-01-18 19:51:45 +1100

    added content to about page and changed navbar

--- react-portfolio/src/components/pages/Home.js
+++ react-portfolio/src/components/pages/Home.js
@@ -1,20 +1,34 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import Typical from 'react-typical';
+import profile from '../../assets/images/profile.jpg';
 
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
+      <Container>
+        <Row >
+          <Col>
+            <h1 className='primary-text'>
+              Hello, I'm <span className='highlighted-text'>Ygor</span>
+            </h1>
+          </Col>
+          <Col>
+            <img src={profile} alt='profile picture' />
+          </Col>
+        </Row>
+      </Container>
+      <h2>
+        <Typical loop={Infinity} 
+        steps={["Why do Java programmers have to wear glasses? Because they don’t C#.",1000,
+        "A SQL query goes into a bar, walks up to two tables, and asks, ‘Can I join you?’",1000,
+        "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.", 1000 ]} />
+      </h2>
+      <p>I am passionate and dedicated about the IT industry.
+         I chose IT as my profession as it offers endless possibilities that excite me.
+          Whilst challenges arise I am driven by my growth mindset to overcome and resolve complications.
+           I have worked on my professional development undertaking cloud computing courses.
+            My commitment to life long learning and passion for the profession motivates me to grow professionally and personally.
       </p>
     </div>
   );
